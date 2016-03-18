af38279957f9a4bd5853c30f9a999d76fab4ff92
dusqiu <dusqiu@163.com>
2016-03-18 12:12:16 +0800

    Update server.php

--- server.php
+++ server.php
@@ -8,7 +8,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
+//hello
 $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
 if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
